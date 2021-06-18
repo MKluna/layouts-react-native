8025340ab097377880d30294d2507216a7c4d2a9
MKluna <matiaslunastyles@gmail.com>
2021-06-18 11:59:57 -0300

    init

---
+++ App.js
@@ -0,0 +1,124 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {StyleSheet, View, Image, Text, ScrollView} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <ScrollView>
+        <View style={{flexDirection: 'row'}}>
+          <Image
+            source={require('./assets/img/bg.jpg')}
+            style={styles.banner}
+          />
+        </View>
+        <View style={styles.contenedor}>
+          <Text style={styles.titulo}>Qué hacer en Paris</Text>
+          <ScrollView horizontal>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img_two/actividad1.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img_two/actividad2.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img_two/actividad3.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img_two/actividad4.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img_two/actividad5.jpg')}
+              />
+            </View>
+          </ScrollView>
+          <Text style={styles.titulo}>Los Mejores Alojamientos</Text>
+          <View>
+            <View>
+              <Image
+                style={styles.mejores}
+                source={require('./assets/img/mejores1.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.mejores}
+                source={require('./assets/img/mejores2.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.mejores}
+                source={require('./assets/img/mejores3.jpg')}
+              />
+            </View>
+          </View>
+          <Text style={styles.titulo}>Hospedajes en LA</Text>
+          <View style={styles.listado}>
+            <View style={styles.listadoItem}>
+              <Image
+                style={styles.mejores}
+                source={require('./assets/img_three/hospedaje1.jpg')}
+              />
+            </View>
+            <View style={styles.listadoItem}>
+              <Image
+                style={styles.mejores}
+                source={require('./assets/img_three/hospedaje2.jpg')}
+              />
+            </View>
+            <View style={styles.listadoItem}>
+              <Image
+                style={styles.mejores}
+                source={require('./assets/img_three/hospedaje3.jpg')}
+              />
+            </View>
+            <View style={styles.listadoItem}>
+              <Image
+                style={styles.mejores}
+                source={require('./assets/img_three/hospedaje4.jpg')}
+              />
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  banner: {
+    height: 250,
+    flex: 1,
+  },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginVertical: 20,
+  },
+  contenedor: {marginHorizontal: 10},
+  ciudad: {width: 250, height: 300, marginRight: 10},
+  mejores: {width: '100%', height: 200, marginVertical: 5},
+  listado: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  listadoItem: {flexBasis: '49%'},
+});
+
+export default App;
